e1121d2eeb3beb4366877dcbb340aea08e6efa88
ideyedi <gdregas18@gmail.com>
2022-07-02 16:40:28 +0900

    crawler base 추가

---
+++ extractInstaApi/api/util/crawler.py
@@ -0,0 +1,58 @@
+urls.URL = 'https://www.instagram.com/p/CfVx-zFvcg7'
+class instacrawler(urls.URL):
+    def __init__(self, url):
+        super().__init__(url)
+        self.url = url
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+        }
+        self.session = requests.Session()
+        self.session.headers = self.headers
+
+    def get_html(self):
+        response = self.session.get(self.url)
+        return response.text
+
+    def get_json(self):
+        response = self.session.get(self.url)
+        return response.json()
+
+    def get_text(self):
+        response = self.session.get(self.url)
+        return response.text
+
+    def get_status_code(self):
+        response = self.session.get(self.url)
+        return response.status_code
+
+    def get_headers(self):
+        response = self.session.get(self.url)
+        return response.headers
+
+    def get_cookies(self):
+        response = self.session.get(self.url)
+        return response.cookies
+
+    def get_history(self):
+        response = self.session.get(self.url)
+        return response.history
+
+    def get_encoding(self):
+        response = self.session.get(self.url)
+        return response.encoding
+
+    def get_content(self):
+        response = self.session.get(self.url)
+        return response.content
+
+    def get_raw_headers(self):
+        response = self.session.get(self.url)
+        return response.raw.headers
+
+    def get_raw_content(self):
+        response = self.session.get(self.url)
+        return response.raw.read()
+
+    def get_raw_text(self):
+        response = self.session.get(self.url)
+        return response.raw.read().decode('utf-8')
--- extractInstaApi/api/views.py
+++ extractInstaApi/api/views.py
@@ -12,7 +12,7 @@ from rest_framework.response import Response
 class DemoApi(views.APIView):
     test_link = 'https://www.instagram.com/p/CfVx-zFvcg7'
     # example 'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/291118957_421835559823293_4126431104790276899_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=vUUG6mjB_wIAX9uXrIf&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg3MDQyMDE1MzAxNzY3MDc4MQ%3D%3D.2-ccb7-5&oh=00_AT_d2DKJYHyG3yCe2McnHb8HkKoZHtzhCFSmORvuTJ2bKQ&oe=62C628D2&_nc_sid=30a2ef'
-    
+
     # /api/extract
     # GET method test
     def get(self, *args):
